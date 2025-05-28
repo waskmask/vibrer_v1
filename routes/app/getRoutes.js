088394f2ac014c8c422b06cb34b6b797856254c9
Waseem <wewe4hyd@gmail.com>
2025-05-28 12:09:45 +0200

    Changes in imprint and terms page

--- routes/app/getRoutes.js
+++ routes/app/getRoutes.js
@@ -940,18 +940,18 @@ router.get("/imprint", function (req, res) {
   });
 });
 
-router.get("/refund-policy", function (req, res) {
-  const language = req.cookies.i18n;
-  let showRefundLangNotice = false;
-  if (language && language !== "en") {
-    showRefundLangNotice = true;
-  }
-  res.render("refund-policy", {
-    title: "Refund policy",
-    path: "/refund",
-    showRefundLangNotice: showRefundLangNotice,
-  });
-});
+// router.get("/refund-policy", function (req, res) {
+//   const language = req.cookies.i18n;
+//   let showRefundLangNotice = false;
+//   if (language && language !== "en") {
+//     showRefundLangNotice = true;
+//   }
+//   res.render("refund-policy", {
+//     title: "Refund policy",
+//     path: "/refund",
+//     showRefundLangNotice: showRefundLangNotice,
+//   });
+// });
 
 // my profile
 router.get("/app/my-profile", function (req, res) {
